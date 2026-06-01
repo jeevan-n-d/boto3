c094e57eee9d5bd5b3b64256cdcaf2629e223ccf
JEEVAN N D <jeevanrajeshgowda@gmail.com>
2026-06-01 09:56:40 +0530

    Create context.py

---
+++ context.py
@@ -0,0 +1,22 @@
+def lambda_handler(event, context):
+
+    print("Function Name:", context.function_name)
+
+    print("Function Version:", context.function_version)
+
+    print("Request ID:", context.aws_request_id)
+
+    print("Memory:", context.memory_limit_in_mb)
+
+    print("Log Group:", context.log_group_name)
+
+    print("Log Stream:", context.log_stream_name)
+
+    print("ARN:", context.invoked_function_arn)
+
+    print("Remaining Time:",
+          context.get_remaining_time_in_millis())
+
+    return {
+        "statusCode": 200
+    }
